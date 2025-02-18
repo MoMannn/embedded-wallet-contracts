b29717a26d37d0a9b4c61388fdc2781de180a92d
Mitja Kjuder <mitja.kjuder@gmail.com>
2025-02-18 14:29:30 +0100

    comments added in solidity code

---
+++ contracts/interfaces/IAccount.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: Apache-2.0
+
+pragma solidity ^0.8.0;
+
+interface IAccount {
+    function createWallet (
+        bytes32 keypairSecret
+    ) external returns (bytes32);
+
+    function removeWallet (
+        uint256 walletId
+    ) external;
+}
