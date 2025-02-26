06a14bb7ae80e4e1b6159252fd1ccc15f18ec599
Mitja Kjuder <mitja.kjuder@gmail.com>
2025-02-26 13:15:57 +0100

    added WalletCreate emit event at first wallet creation

--- scripts/helper/get-tx-data.ts
+++ scripts/helper/get-tx-data.ts
@@ -1,20 +1,21 @@
-import { ethers } from "ethers";
 import hre from "hardhat";
 
 async function main() {
 
-  const accountManagerAddress = "0x510518EBe8266fDF6858d2852ADA3bfE50988DAB";
+    const accountManagerAddress = "0x510518EBe8266fDF6858d2852ADA3bfE50988DAB";
+    const accountEVM = "0x66511004f94e7832465fcF3fec59d5467B8dD3bC";
 
-  const signer = (await hre.ethers.getSigners())[0];
-  const contract = await hre.ethers.getContractAt('AccountManager', accountManagerAddress, signer);
+    const signer = (await hre.ethers.getSigners())[0];
+    const contract = await hre.ethers.getContractAt('AccountManager', accountManagerAddress, signer);
+    const contractAcc = await hre.ethers.getContractAt('AccountEVM', accountEVM, signer);
 
-  const hash = "0x6d7f538e2d0d64ebab65f24595cf5e4d4d769f93ea3247dfa182830a755539cb";
+    const hash = "0x159a7181b83f65d1a2321378f1163322d8f08e2b11e1ac82ccc25c15fbd6d1d7";
 
-  const receipt = await signer.provider.getTransactionReceipt(hash);
-  const result = receipt.logs.map((log) => contract.interface.parseLog(log));
+    const receipt = await signer.provider.getTransactionReceipt(hash);
 
-  console.log(result);
+    const result = receipt.logs.map((log) => contract.interface.parseLog(log) ?? contractAcc.interface.parseLog(log));
 
+    console.log(result);
 }
 
 main()
